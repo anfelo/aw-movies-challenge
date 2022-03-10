7a233e4d3d304ab4d8381853611dd2dceae209a4
Andres Osorio <af.osorio1341@gmail.com>
2022-03-10 11:48:14 -0500

    Cleanded up subscription after component is destroyed

--- src/app/modules/movies/movies-list/movies-list.component.ts
+++ src/app/modules/movies/movies-list/movies-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Movie } from '../interfaces/movie';
 import { MoviesService } from '../movies.service';
 
@@ -7,14 +8,19 @@ import { MoviesService } from '../movies.service';
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.scss']
 })
-export class MoviesListComponent implements OnInit {
+export class MoviesListComponent implements OnInit, OnDestroy {
   moviesGroupsByYear: Movie[][] = ([] as Movie[][]);
+  moviesByYearSub: Subscription = new Subscription();
 
   constructor(private moviesService: MoviesService) { }
 
   ngOnInit(): void {
-    this.moviesService.moviesGroupsByYear$.subscribe(moviesGroups => {
+    this.moviesByYearSub = this.moviesService.moviesGroupsByYear$.subscribe(moviesGroups => {
       this.moviesGroupsByYear = moviesGroups;
     })
+  }
+
+  ngOnDestroy(): void {
+    this.moviesByYearSub.unsubscribe();
   }
 }
