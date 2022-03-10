5d803c9210b2d5aa66f7b48b163b743341b307e0
Andres Osorio <af.osorio1341@gmail.com>
2022-03-10 05:42:41 -0500

    Added bulma for base styles

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('aw-movies-challenge');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('aw-movies-challenge app is running!');
-  });
 });
